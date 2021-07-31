74de49288cc95f5438d0fc6b674723f7718fde9e
omodolapo-williams <88205624+omodolapo-williams@users.noreply.github.com>
2021-07-31 14:04:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let celsius = document.getElementById("celsius");
+
+let fahrenheit = document.getElementById('fahrenheit');
+
+function farCel(){
+  let output = (parseFloat(celsius.value)* 9/5 ) +
+  32;
+
+  fahrenheit.value = parseFloat(output.toFixed(2));
+
+console.log(output);
+}
+
+function celToFar() {
+
+   let output = (parseFloat(fahrenheit.value) - 32 ) *
+   5/9;
+
+  celsius.value = parseFloat (output.toFixed(2));
+
+  console.log(output);
+}
